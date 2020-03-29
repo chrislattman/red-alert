c973e64cafd41645ed780ba0a3a0fb623e39b759
stratosphericvibes <tanyav2@illinois.edu>
2020-03-28 19:51:31 -0500

    outline calls to database

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 '''
 This is the public API that we share with other websites/apps.
 '''
+import db # this is just whatever db we end up using 
 
 def flag_suspicious_phone(phone):
     pass
@@ -9,13 +10,20 @@ def flag_suspicious_address(addr):
     pass
 
 def flag_suspicious_image(img):
+    # compares to hash using https://perception.thorn.engineering/en/latest/index.html api
     pass
 
 def flag_suspicious_email(email):
-    print(email)
+    # flag based on both the email string and the email domain
+    domain = email.split('@')[1]
+    domain_sus = db.is_domain_suspicious(domain)
+    email_sus = db.is_email_suspicious(email)
+
+    # return relevant info as json
+    return (domain_sus, email_sus)
 
 def flag_suspicious_ip(ip):
-    pass
+    return db.is_ip_suspicious(ip)
 
 def flag_suspicious_listing(listing):
     # this will call all the above functions
---
+++ db.py
@@ -0,0 +1,12 @@
+'''
+This file includes actually calling the database 
+'''
+
+def is_domain_suspicious(domain):
+    pass
+
+def is_email_suspicious(email):
+    pass
+
+def is_ip_suspicious(ip):
+    pass
