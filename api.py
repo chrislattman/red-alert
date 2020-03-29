f0c1478092b1a4a45084f1e9b413c13be41843da
stratosphericvibes <tanyav2@illinois.edu>
2020-03-28 19:19:21 -0500

    barebones public api

---
+++ api.py
@@ -0,0 +1,22 @@
+'''
+This is the public API that we share with other websites/apps.
+'''
+
+def flag_suspicious_phone(phone):
+    pass
+
+def flag_suspicious_address(addr):
+    pass
+
+def flag_suspicious_image(img):
+    pass
+
+def flag_suspicious_email(img):
+    pass
+
+def flag_suspicious_ip(ip):
+    pass
+
+def flag_suspicious_listing(listing):
+    # this will call all the above functions
+    pass
